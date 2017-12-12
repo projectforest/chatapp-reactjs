f3857b17baf9bd027cfafbd0e27b5d5cb565d93a
hlinforest <hllin@sfu.ca>
2017-12-11 21:22:54 -0800

    change username

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -8,18 +8,25 @@ class ChatBar extends Component {
 
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleContentChange = this.handleContentChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleContentSubmit = this.handleContentSubmit.bind(this);
   }
 
   handleNameChange = (event) => {
     this.setState({username: event.target.value});
   }
 
+  handleNameSubmit = (event) => {
+    if(event.key === "Enter") {
+      this.props.currentUser(this.state);
+      this.setState({username: event.target.value});
+    }
+  }
+
   handleContentChange = (event) => {
     this.setState({content: event.target.value});
   }
 
-  handleSubmit = (event) => {
+  handleContentSubmit = (event) => {
     if(event.key === "Enter") {
       this.props.handleInputMessage(this.state);
       this.setState({content: ''});
@@ -30,8 +37,8 @@ class ChatBar extends Component {
     console.log('chatbar this.props', this.props);
     return(
       <footer className="chatbar">
-      <input className="chatbar-username" type="text" value={this.state.username} onChange={this.handleNameChange}/>
-      <input className="chatbar-message" type="text" placeholder="Type a message and hit ENTER" value={this.state.content} onChange={this.handleContentChange} onKeyPress={this.handleSubmit}/>
+        <input className="chatbar-username" type="text" value={this.state.username} onChange={this.handleNameChange} onKeyPress={this.handleNameSubmit}/>
+        <input className="chatbar-message" type="text" placeholder="Type a message and hit ENTER" value={this.state.content} onChange={this.handleContentChange} onKeyPress={this.handleContentSubmit}/>
       </footer>
     );
   }
